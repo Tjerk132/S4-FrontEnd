339419c7edff7027b9a5203a8f4aba7e99ac1787
i386599 <t.sevenich@student.fontys.nl>
2020-06-21 21:46:16 +0200

    adjusted html base & few src test

--- view/home/Home.vue
+++ view/home/Home.vue
@@ -12,7 +12,7 @@
     </marquee>
 
     <div>  
-      <img :src="'./images/warehouse.png'" class="warehouse-img">
+      <img :src="'images/warehouse.png'" class="warehouse-img">
       <top-rated-suggestions/>
 
       <div class="payment-options">
